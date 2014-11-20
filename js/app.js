f4e40baf66032b2bce0e4b4166ef2b771f6e2ebd
derekkoch <derek@freeyourdata.net>
2014-11-20 16:14:25 -0500

    Play features, minus reset

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
+
+	alert("Welcome to Tic Tac Toe. Player X, begin.");
+
 	var counter = 1;
 
 	$(document).on('click', '.square', function() {
-		console.log("Square clicked.");
+		
+		if (counter % 2 == 0) {
+			$(this).children(".squareValue").html("O");
+				counter = counter + 1;
+				alert("Player X, your move.");
+			} else {
+				$(this).children(".squareValue").html("X");
+				counter = counter + 1;
+				alert("Player O, your move");
+			}
 	});
 
 	$(document).on('click', '#reset', function() {
-		console.log("Gameboard reset.");
+		alert("Gameboard reset.");
+		counter = 1;
+
 	});
 
 });
